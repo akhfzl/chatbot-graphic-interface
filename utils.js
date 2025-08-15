8e406834238dca33cd43b7a22a344498f5f9935b
akhfzl <akhmadfaizal.works@gmail.com>
2025-08-15 19:21:26 +0700

    'dynamic'

--- utils.js
+++ utils.js
@@ -1,10 +1,41 @@
 const CRISP_BASE_URL = "https://api.crisp.chat/v1/website";
 const WEBSITE_ID = "2bd940f6-2190-436e-bbb0-1d419f2dd437";
-const SESSION_ID = "session_b1b3d6fa-058b-4aa2-b14c-8bf11f6b1f1e";
 const CRISP_AUTH =
   "Basic M2M4YmEwNDktZWNmZi00NjVlLWIyOTEtYWI2NWM2NTlkOGZhOjM3Y2M1NmQ5NTNiZTkxYjFlOGZiNGMxNTMxZGIzZGFhZWE1YWJhZTkzOTFiYmJjYTYwZTY3ZjE3ODk2OTU1YmY=";
 
+async function createCrispConversation() {
+  try {
+    const res = await fetch(`${CRISP_BASE_URL}/${WEBSITE_ID}/conversation`, {
+      method: "POST",
+      headers: {
+        Authorization: CRISP_AUTH,
+        "X-Crisp-Tier": "plugin",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}), // kosongkan body sesuai API Crisp
+    });
+
+    if (!res.ok) {
+      throw new Error(`Gagal membuat conversation: ${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    console.log("Conversation berhasil dibuat:", data);
+    return data;
+  } catch (err) {
+    console.error("Error:", err.message);
+    throw err;
+  }
+}
+
 async function getCrispConversation() {
+  const inisiateSession = createCrispConversation();
+  const SESSION_ID = inisiateSession && inisiateSession.data && inisiateSession.data.session_id ? inisiateSession.data.session_id : null;
+
+  if (!SESSION_ID) {
+    throw "Kesalahan Sistem";
+  }
+
   const res = await fetch(`${CRISP_BASE_URL}/${WEBSITE_ID}/conversation/${SESSION_ID}`, {
     method: "GET",
     headers: {
@@ -17,6 +48,13 @@ async function getCrispConversation() {
 }
 
 async function sendMessageToCrisp(content) {
+  const inisiateSession = createCrispConversation();
+  const SESSION_ID = inisiateSession && inisiateSession.data && inisiateSession.data.session_id ? inisiateSession.data.session_id : null;
+
+  if (!SESSION_ID) {
+    throw "Kesalahan Sistem";
+  }
+
   const res = await fetch(`${CRISP_BASE_URL}/${WEBSITE_ID}/conversation/${SESSION_ID}/message`, {
     method: "POST",
     headers: {
