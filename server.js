fd9ca3ae375de1bb2d816446c8175fc1f20ec757
akhfzl <akhmadfaizal.works@gmail.com>
2025-08-20 13:17:13 +0700

    'final'

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
-const { getCrispConversation, sendMessageToCrisp } = require("./utils");
+const { getVisitorTokenByIP, sendMessageToCrispBySession } = require("./utils");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/chat", async (req, res) => {
-  try {
-    const response = await fetch("https://n8n.smartid.or.id/webhook/chatbot", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(req.body),
-    });
+const CRISP_BASE_URL = "https://api.crisp.chat/v1/website";
+const WEBSITE_ID = "2bd940f6-2190-436e-bbb0-1d419f2dd437";
+const CRISP_AUTH =
+  "Basic M2M4YmEwNDktZWNmZi00NjVlLWIyOTEtYWI2NWM2NTlkOGZhOjM3Y2M1NmQ5NTNiZTkxYjFlOGZiNGMxNTMxZGIzZGFhZWE1YWJhZTkzOTFiYmJjYTYwZTY3ZjE3ODk2OTU1YmY=";
 
-    const data = await response.json();
-    res.json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Gagal menghubungi chatbot" });
-  }
-});
+const lastProcessed = new Map();
 
 app.get("/process-crisp", async (req, res) => {
   try {
-    const conversation = await getCrispConversation();
-    const N8N_WEBHOOK = "https://n8n.smartid.or.id/webhook/chatbot";
+    const sessionId = req.query.sessionId;
+    if (!sessionId) {
+      return res.status(400).json({ error: "IP tidak ditemukan" });
+    }
+
+    const resConv = await fetch(`${CRISP_BASE_URL}/${WEBSITE_ID}/conversation/${sessionId}`, {
+      headers: {
+        Authorization: CRISP_AUTH,
+        "X-Crisp-Tier": "plugin",
+      },
+    });
+    const conversation = await resConv.json();
 
     const { preview_message, last_message } = conversation.data || {};
-
     if (preview_message?.from === "user") {
+      // const lastId = lastProcessed.get(sessionId);
+      // if (lastId === last_message?.id) {
+      //   return res.json({ status: "ignored", reason: "Pesan sudah diproses sebelumnya", users: lastProcessed });
+      // }
+
+      const N8N_WEBHOOK = "https://n8n.smartid.or.id/webhook/chatbot";
       const webhookRes = await fetch(N8N_WEBHOOK, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -38,8 +46,7 @@ app.get("/process-crisp", async (req, res) => {
       const webhookData = await webhookRes.json();
 
       const botReply = webhookData.output || "⚠️ Bot tidak memberikan balasan.";
-
-      await sendMessageToCrisp(botReply);
+      await sendMessageToCrispBySession(sessionId, CRISP_BASE_URL, WEBSITE_ID, CRISP_AUTH, botReply);
 
       res.json({ status: "success", sent: botReply });
     } else {
@@ -51,10 +58,10 @@ app.get("/process-crisp", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server berjalan di https://chatbot-smartid-app.vercel.app/");
-});
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server berjalan di http://localhost:3000");
 });
--- utils.js
+++ utils.js
@@ -1,61 +1,25 @@
-const CRISP_BASE_URL = "https://api.crisp.chat/v1/website";
-const WEBSITE_ID = "2bd940f6-2190-436e-bbb0-1d419f2dd437";
-const CRISP_AUTH =
-  "Basic M2M4YmEwNDktZWNmZi00NjVlLWIyOTEtYWI2NWM2NTlkOGZhOjM3Y2M1NmQ5NTNiZTkxYjFlOGZiNGMxNTMxZGIzZGFhZWE1YWJhZTkzOTFiYmJjYTYwZTY3ZjE3ODk2OTU1YmY=";
-
-async function createCrispConversation() {
-  try {
-    const res = await fetch(`${CRISP_BASE_URL}/${WEBSITE_ID}/conversation`, {
-      method: "POST",
-      headers: {
-        Authorization: CRISP_AUTH,
-        "X-Crisp-Tier": "plugin",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({}), // kosongkan body sesuai API Crisp
-    });
-
-    if (!res.ok) {
-      throw new Error(`Gagal membuat conversation: ${res.status} ${res.statusText}`);
-    }
-
-    const data = await res.json();
-    console.log("Conversation berhasil dibuat:", data);
-    return data;
-  } catch (err) {
-    console.error("Error:", err.message);
-    throw err;
-  }
-}
-
-async function getCrispConversation() {
-  const inisiateSession = createCrispConversation();
-  const SESSION_ID = inisiateSession && inisiateSession.data && inisiateSession.data.session_id ? inisiateSession.data.session_id : null;
-
-  if (!SESSION_ID) {
-    throw "Kesalahan Sistem";
-  }
-
-  const res = await fetch(`${CRISP_BASE_URL}/${WEBSITE_ID}/conversation/${SESSION_ID}`, {
-    method: "GET",
+async function getVisitorTokenByIP(CRISP_AUTH, WEBSITE_ID, CRISP_BASE_URL, ip) {
+  const url = `${CRISP_BASE_URL}/${WEBSITE_ID}/conversations?search=${ip}`;
+  const res = await fetch(url, {
     headers: {
       Authorization: CRISP_AUTH,
       "X-Crisp-Tier": "plugin",
     },
   });
-  if (!res.ok) throw new Error(`Gagal GET Crisp: ${res.statusText}`);
-  return res.json();
-}
 
-async function sendMessageToCrisp(content) {
-  const inisiateSession = createCrispConversation();
-  const SESSION_ID = inisiateSession && inisiateSession.data && inisiateSession.data.session_id ? inisiateSession.data.session_id : null;
+  const data = await res.json();
 
-  if (!SESSION_ID) {
-    throw "Kesalahan Sistem";
+  if (!data?.data?.length) {
+    return null;
   }
 
-  const res = await fetch(`${CRISP_BASE_URL}/${WEBSITE_ID}/conversation/${SESSION_ID}/message`, {
+  const lastItem = data.data[data.data.length - 1];
+  return lastItem?.meta?.token_id || null;
+}
+
+async function sendMessageToCrispBySession(sessionId, CRISP_BASE_URL, WEBSITE_ID, CRISP_AUTH, content) {
+  const url = `${CRISP_BASE_URL}/${WEBSITE_ID}/conversation/${sessionId}/message`;
+  const res = await fetch(url, {
     method: "POST",
     headers: {
       Authorization: CRISP_AUTH,
@@ -74,6 +38,6 @@ async function sendMessageToCrisp(content) {
 }
 
 module.exports = {
-  getCrispConversation,
-  sendMessageToCrisp,
+  getVisitorTokenByIP,
+  sendMessageToCrispBySession,
 };
